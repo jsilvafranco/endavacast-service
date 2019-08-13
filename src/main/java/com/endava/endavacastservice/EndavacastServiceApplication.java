48f30f819d0ea66fc43c7360a8a83d70b6029141
Jairo Silva <jairo.silva.consultant@sothebys.com>
2019-08-13 16:42:04 -0500

    initial structure

---
+++ src/main/java/com/endava/endavacastservice/EndavacastServiceApplication.java
@@ -0,0 +1,13 @@
+package com.endava.endavacastservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndavacastServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndavacastServiceApplication.class, args);
+	}
+
+}
